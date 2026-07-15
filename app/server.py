a186c88a3b8d9ffe793253726611e424710dd2c0
tuyetntintern-a11y <tuyetnt.intern@vnext.vn>
2026-07-15 18:14:42 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
 @app.get("/api/health")
 def health_check():
-    return "NGUYEN THI TUYET chao anh Nguyễn Quang Tùng"
+    return {
+        "status": "ok"
+    }
