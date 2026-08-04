c9d727f29af4fbc5cc2811d178b8fa8ae37b333f
Nguyen Thi Tuyet <tuyetnt.intern@vnext.vn>
2026-08-04 13:02:11 +0900

    bai1: hello world FastAPI

--- test.py
+++ test.py
@@ -18,3 +18,78 @@ print(tinh_array(array))
 2. Cho 1 dãy số a. Hãy trả về dãy gồm các 
 phần tử chia hết cho 2, 3 và 5 nhưng không chia hết cho 4.
 """
+def find_numbers(array):
+    result = []
+    for i in range(len(array)):
+        if array[i] % (2*3*5) == 0 and array[i] % 4 != 0:
+            result.append(array[i])
+
+    return result
+array = [1,3,5,7,9,30,60]
+print(*find_numbers(array))
+# dau * de giai nen, tuc la chi lay phan tu ben trong list
+
+
+"""
+
+3.
+  | 1 | 2 | 3 | 4 | 5 | 6
+-------------------------
+1 | x | x | o | o | x | x
+-------------------------
+2 | x | x | o | o | x | x
+-------------------------
+3 | x | o | o | o | o | x
+-------------------------
+4 | x | x | o | o | x | x
+-------------------------
+5 | x | x | o | o | x | x
+-------------------------
+6 | x | x | o | o | x | x
+Cho 2 số nguyên r và c với 1 <= r, c <= 6. Nếu ô ở hàng r và cột c trong bảng cố định trên là 'o' 
+thì trả về True, là 'x' thì trả về False.
+
+"""
+def check_matrix(r, c):
+    if c == 3 or c == 4 or (r == 3 and ((c == 2) or (c == 5))):
+        return True
+    
+"""
+4. Cho 3 số nguyên n, m, r > 0. Trả về dãy số gồm tất cả các số nguyên nhỏ hơn n và khi chia 
+cho m thì có số dư bằng r.
+
+"""
+def find_number(n, m, r):
+    result = []
+    for i in range(1, n):
+        if i % m == r:
+            result.append(i)
+    return result
+"""
+5. Bạn được cho:
+
+Một mảng nguyên dương a biểu thị mức độ tham lam của mỗi đứa trẻ.
+Một mảng nguyên dương b biểu thị kích thước của các chiếc bánh quy.
+
+Mỗi đứa trẻ chỉ có thể nhận tối đa một chiếc bánh quy.
+
+Một đứa trẻ được xem là hài lòng nếu kích thước của chiếc bánh quy được phát cho nó lớn hơn hoặc bằng 
+mức độ tham lam của đứa trẻ đó.
+
+Hãy trả về số lượng lớn nhất các đứa trẻ có thể được làm hài lòng.
+
+"""
+def sort_arrayA(arrayA):
+    arrayA_res = arrayA.sorted()
+    return arrayA_res
+def sort_arrayB(arrayB):
+    arrayB_res = arrayB.sorted()
+    return arrayB_res
+def dua_tre_hai_long(arrayA_res, arrayB_res):
+    result = 0
+    for i in arrayA_res:
+        for j in arrayB_res:
+            if arrayB_res[j] >= arrayA_res[i]:
+                result += 1
+    return result
+
